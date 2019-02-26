5bc42e9a213705d6b447dd28bbb751ce78cde276
choi8686 <this0924@gmail.com>
2019-02-26 21:53:13 +0900

    fix stop and start, increase & decrease minutes

--- timer.js
+++ timer.js
@@ -5,6 +5,7 @@
   ex) 25min -> 5min -> 25min*/
 
 var startBtn = document.querySelector("#start-btn");
+var stopBtn = document.querySelector("#stop-btn");
 var resetBtn = document.querySelector("#reset-btn");
 var workMin =  document.querySelector("#work-min")
 var breakMin =  document.querySelector("#break-min")
@@ -18,38 +19,49 @@ var seconds = 1500;
 var workTime = 25;
 var breakTime = 5;
 var countdown = 0;
-var interval = setInterval(timer, 1000);
 var isPaused = true;
+setInterval(timer, 1000);
 
+startBtn.addEventListener('click', clickStart);
+stopBtn.addEventListener('click', clickStop);
+resetBtn.addEventListener('click', clickReset);
+workPlus.addEventListener('click', setWorkTime);
+workMinus.addEventListener('click', setWorkTime);
 
 function convertSeconds(s){
   var min = Math.floor(s / 60);
-  var sec = s % 60;
-  return min + ":" + sec
+  var sec = ("0" + s % 60).slice(-2);
+  return min + ":" + sec;
 }
 
 function timer(){
-  countdown++;
-  display.innerText = convertSeconds(seconds - countdown);
-}
-
-function clickStart(){
-  interval;
-  isPaused = false;
-}
-
-function clickStop(){
-  clearInterval(interval);
-  isPaused = true;
-  if(isPaused === true){
-    clickStart();
+  if(isPaused == false){
+    countdown++;
+    display.innerText = convertSeconds(seconds - countdown);
   }
 }
 
+function clickStart(){
+  isPaused = false;
+  //startBtn.innerText = "STOP"
+}
 
+function clickStop(){
+  isPaused = true;
+}
 
+function clickReset(){
+  countdown = 0;
+  isPaused = true;
+  display.innerText = convertSeconds(seconds - countdown);
+}
 
-
-function init(){
-
+function setWorkTime(event){
+  if(event.target.innerText === "+"){
+    seconds += 60;
+  } else if(event.target.innerText === "-"){
+    seconds -= 60;
+  }
+  workMin.innerText = seconds / 60;
+  display.innerText = convertSeconds(seconds - countdown);
 }
